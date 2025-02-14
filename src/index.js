e66b89d5a3d14ad7d17c03388b69a9f4e79e3fd1
iiblacksmithii <94884566+iiiblacksmithiii@users.noreply.github.com>
2025-02-14 14:27:24 -0700

    added things

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+const root = document.getElementById("root");
+if (!root) {
+  console.error("React Error: No element with id 'root' found in index.html");
+}
+
+const reactRoot = ReactDOM.createRoot(root);
+reactRoot.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>

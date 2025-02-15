49367f9f2025e75b45e929e84c5ba51d1bc1b4a5
iiblacksmithii <94884566+iiiblacksmithiii@users.noreply.github.com>
2025-02-15 13:52:40 -0700

    work please

--- web4350capstone/urls.py
+++ web4350capstone/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from django.http import HttpResponse  # Add this line
 
 def homepage(request):
-    return HttpResponse("<h1>Welcome to Web4350Capstone Project!23</h1>")
+    return HttpResponse("<h1>Welcome to Web4350Capstone Project!234</h1>")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
